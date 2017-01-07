acc0f913c66825983bc295695c32aedcf58d6e58
timathom <timathom@gmail.com>
2017-01-07 18:50:13 -0500

    Update.

--- discovery/resources/js/simple-statistics-2.2.0/scripts/update_readme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-var fs = require('fs');
-
-var version = JSON.parse(fs.readFileSync('./package.json')).version;
-var README = fs.readFileSync('./README.md', 'utf8');
-fs.writeFileSync(
-    'README.md',
-    README.replace(/simple-statistics@([\d\.]+)/g,
-    'simple-statistics@' + version));
